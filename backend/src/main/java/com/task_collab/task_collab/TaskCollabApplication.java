b1bb3732a0bca56eeded1152a71fcf56f8361549
andreeavodita <andreea.vodita11@gmail.com>
2026-01-06 19:01:26 +0200

    feat: lets see if idea works

---
+++ backend/src/main/java/com/task_collab/task_collab/TaskCollabApplication.java
@@ -0,0 +1,13 @@
+package com.task_collab.task_collab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCollabApplication.class, args);
+	}
+
+}
