2be5d31b5ade9b88320f53e8153bc8da623a7d3e
andreeavodita <andreea.vodita11@gmail.com>
2026-01-08 22:01:46 +0200

    feat: service layer done.

---
+++ backend/src/main/java/com/task_collab/services/ListService.java
@@ -0,0 +1,82 @@
+package com.task_collab.services;
+
+import com.task_collab.entities.ListEntity;
+import com.task_collab.repositories.ListRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ListService {
+
+    private final ListRepository listRepository;
+
+    public ListService(final ListRepository listRepository) {
+        this.listRepository = listRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ListEntity> getAllLists() {
+        return listRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public ListEntity getList(UUID id) {
+        return listRepository.findById(id).orElseThrow();
+    }
+
+    @Transactional
+    public void createList(final String title) {
+        ListEntity list = new ListEntity(title);
+        listRepository.save(list);
+    }
+
+    @Transactional
+    public void deleteList(UUID listId) {
+        listRepository.deleteById(listId);
+    }
+
+    @Transactional
+    public void renameList(final UUID listId, final String title) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.rename(title);
+    }
+
+    @Transactional
+    public void addItem(final UUID listId, final String name) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.addItem(name);
+    }
+
+    @Transactional
+    public void softDeleteItem(final UUID listId, final UUID itemId) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.softDeleteItem(itemId);
+    }
+
+    @Transactional
+    public void hardDeleteItem(final UUID listId, final UUID itemId) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.hardDeleteItem(itemId);
+    }
+
+    @Transactional
+    public void archiveItem(final UUID listId, final UUID itemId) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.archiveItem(itemId);
+    }
+
+    @Transactional
+    public void markItemDone(final UUID listId, final UUID itemId) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.markItemDone(itemId);
+    }
+
+    @Transactional
+    public void markItemActive(final UUID listId, final UUID itemId) {
+        ListEntity list = listRepository.findById(listId).orElseThrow();
+        list.markItemActive(itemId);
+    }
+}
