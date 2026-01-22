363a5ae5bb7feb1cc7e14254c2a13f4c830789ed
andreeavodita <andreea.vodita11@gmail.com>
2026-01-22 17:34:50 +0200

    feat: create validation layer

--- backend/src/main/java/com/task_collab/dto/CreateListRequest.java
+++ backend/src/main/java/com/task_collab/dto/CreateListRequest.java
@@ -1,7 +1,10 @@
 package com.task_collab.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateListRequest {
 
+    @NotBlank
     private String title;
 
     public String getTitle() {
